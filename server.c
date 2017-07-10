b299f89fee0c453d55360dd1f2419631caa6ebbf
Justin Azoff <justin.azoff@gmail.com>
2017-07-09 19:40:07 -0500

    more stuff

--- server.c
+++ server.c
@@ -6,6 +6,7 @@ int main(int argn, char *argv[])
     zactor_t *server1 = zactor_new (zsimpledisco, "disco");
     assert (server1);
     zstr_send (server1, "VERBOSE");
+    zstr_sendx (server1, "BIND", "tcp://*:9101", NULL);
     zclock_sleep (3000);
     zactor_destroy(&server1);
 }
--- zsimpledisco.c
+++ zsimpledisco.c
@@ -7,9 +7,22 @@ typedef struct {
     zsock_t *pipe;              //  Actor command pipe
     bool terminated;            //  Did caller ask us to quit?
     bool verbose;               //  Verbose logging enabled?
-    char hostname [NI_MAXHOST]; //  Saved host name
+    zsock_t *server_socket;     //  Socket for talking to clients
+
 } self_t;
 
+static void
+s_self_destroy (self_t **self_p)
+{
+    assert (self_p);
+    if (*self_p) {
+        self_t *self = *self_p;
+        if (self->server_socket) // don't close STDIN
+            zsock_destroy (&self->server_socket);
+        freen (self);
+        *self_p = NULL;
+    }
+}
 
 static self_t *
 s_self_new (zsock_t *pipe)
@@ -17,6 +30,9 @@ s_self_new (zsock_t *pipe)
     self_t *self = (self_t *) zmalloc (sizeof (self_t));
     assert (self);
     self->pipe = pipe;
+
+    self->server_socket = zsock_new (ZMQ_ROUTER);
+
     return self;
 }
 
@@ -33,6 +49,13 @@ s_self_handle_pipe (self_t *self)
 
     if (streq (command, "VERBOSE"))
         self->verbose = true;
+    else
+    if (streq (command, "BIND")) {
+        char *endpoint = zstr_recv (self->pipe);
+        if(-1 == zsock_bind (self->server_socket, "%s", endpoint))
+            zsys_warning ("could not bind to %s", endpoint);
+        zstr_free(&endpoint);
+    }
     else
     if (streq (command, "$TERM"))
         self->terminated = true;
@@ -62,4 +85,5 @@ zsimpledisco (zsock_t *pipe, void *args)
             s_self_handle_pipe (self);
         zsys_debug ("zsimpledisco: Idle");
     }
+    s_self_destroy(&self);
 }
